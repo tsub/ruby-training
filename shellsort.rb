22cf38510be3f19a2fecf91338b05e35508282da
tsubasa <nor.known.to.life.51122@gmail.com>
2014-08-18 18:04:02 +0900

    関数化とコードの簡略化

--- shellsort.rb
+++ shellsort.rb
@@ -1,27 +1,24 @@
 q = [7,3,4,2,1,5,8]
-gap = q.length / 2
 
-print q
-puts
-
-(0..q.length).each do
-	puts gap
-	(gap...q.length).each do |j|
-		(j-gap).downto(0) do |k|
-			if q[k] <= q[k+gap]
-				break
-			else
-				tmp = q[k]
-				q[k] = q[k+gap]
-				q[k+gap] = tmp
-				print q
+def shellSort(seq, gap)
+	(0..seq.length).each do
+		(gap...seq.length).each do |j|
+			(j-gap).downto(0) do |k|
+				if seq[k] <= seq[k+gap]
+					break
+				else
+					seq[k], seq[k+gap] = seq[k+gap], seq[k]
+				end
 			end
 		end
-	end
-	puts
 
-	gap /= 2
-	if gap == 0
-		break
+		gap /= 2
+		if gap == 0
+			break
+		end
 	end
+	return seq
 end
+
+p q
+p shellSort(q, q.length/2)
