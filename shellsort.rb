5ed8b17f9e89c200837f8970ff1ea60876eba8de
tsubasa <nor.known.to.life.51122@gmail.com>
2014-08-19 15:58:33 +0900

    no message

--- shellsort.rb
+++ shellsort.rb
@@ -7,17 +7,13 @@ class Array
 	def shell_sort
 		gap = length / 2
 		while gap > 0
-			print 'gap: ', gap; puts
 			(gap...length).each do |i|
-				print '	i: ', i, ', i-gap: ', i-gap; puts
 				(i-gap).downto(0) do |j|
-					print '		j: ', j, ', j+gap: ', j+gap, ', self[j]: ', self[j], ', self[j+gap]: ', self[j+gap]; puts
 					break if self[j] < self[j+gap]
 					self[j], self[j+gap] = self[j+gap], self[j]
 				end
 			end
 			gap /= 2
-			print 'self: ', self; puts
 		end
 		self
 	end
